baf2e991f8db4aa8adb6dfac23328345c63f6e63
Daniel Kifer <dkifer@cse.psu.edu>
2020-08-04 17:06:04 -0400

    added files for refactoring

---
+++ matrixops.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def is_pos_def(A: np.ndarray):
+    """
+    Returns True if the numpy array A is symmetric positive definite
+    """
+    pd = False
+    # first check symmetry
+    if np.allclose(A, A.T, 1e-5, 1e-8):
+        # check if Cholesky decomposition is successful
+        try:
+            np.linalg.cholesky(A)
+            pd = True
+        except np.linalg.LinAlgError:
+            pd = False
+    return False
